f04767f6f79eea95516809835539c00b14985b9d
Rudra Pratap Singh <144105327+Rudra78996@users.noreply.github.com>
2026-07-11 22:52:40 +0530

    Update header text for deployment feature test

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
       <section id="center">
 
         <div className="hero">
-          <h1>Testing Auto Deployment Feature and container</h1>
+          <h1>Test the branch feature</h1>
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
           <img src={viteLogo} className="vite" alt="Vite logo" />
